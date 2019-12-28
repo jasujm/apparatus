b05d5302ae4a4e9c79b9de75af9d3dfa6a08cefb
Jaakko Moisio <jaakko@moisio.fi>
2019-12-28 15:16:10 +0100

    Create media directory for the apparatus app

--- web/start.sh
+++ web/start.sh
@@ -13,6 +13,12 @@ until nc -z -w30 mysql 3306; do
     waittime=$((waittime<32 ? waittime : 32))
 done
 
+apparatus_media_dir=/var/www/apparatus/media
+if [ ! -d $apparatus_media_dir ]; then
+    mkdir -p $apparatus_media_dir
+    chown uwsgi:uwsgi $apparatus_media_dir
+fi
+
 python manage.py migrate
 python manage.py collectstatic --noinput
 exec uwsgi --uid uwsgi --socket :8000 --protocol uwsgi --module apparatus.wsgi
