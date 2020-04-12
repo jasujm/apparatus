87dda1d1960a6c3aa0fd96048f0f3f8068b7773d
Jaakko Moisio <jaakko@moisio.fi>
2020-04-12 14:17:43 +0200

    Remove separate docker-compose.yml files for dev and prod

--- scripts/run-apparatus.sh
+++ scripts/run-apparatus.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-if [ ! -f docker-compose.prod.yml ]; then
-    echo 'Please ensure that the docker-compose files are present'
-    exit 1;
-fi
-docker-compose -f docker-compose.yml -f docker-compose.certbot.yml -f docker-compose.prod.yml up -d --build
+docker-compose -f docker-compose.yml -f docker-compose.certbot.yml up -d --build
