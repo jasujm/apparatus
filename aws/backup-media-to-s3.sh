c24cb51855d2dd5f59551986531dd470b59746c0
Jaakko Moisio <jaakko@moisio.fi>
2020-04-13 14:51:17 +0200

    Add scripts for uploading backups to S3

---
+++ aws/backup-media-to-s3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+bucket=$APPARATUS_BACKUP_S3_BUCKET
+
+if [ ! $bucket ]; then
+    echo 'APPARATUS_BACKUP_S3_BUCKET not set.'
+fi
+
+scripts/mediadump.sh | gzip | aws s3 cp - s3://${bucket}/mediadump-$(date --iso-8601=seconds).gz --storage-class=ONEZONE_IA
