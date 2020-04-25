0731ef38d3c57d615b54485e8b5f671d2bd5d2d9
Jaakko Moisio <jaakko@moisio.fi>
2020-04-25 09:52:06 +0200

    Change file extension of media dumps to .tgz

--- aws/backup-media-to-s3.sh
+++ aws/backup-media-to-s3.sh
@@ -6,4 +6,4 @@ if [ ! $bucket ]; then
     echo 'APPARATUS_BACKUP_S3_BUCKET not set.'
 fi
 
-scripts/mediadump.sh | gzip | aws s3 cp - s3://${bucket}/mediadump-$(date --iso-8601=seconds).gz --storage-class=ONEZONE_IA
+scripts/mediadump.sh | gzip | aws s3 cp - s3://${bucket}/mediadump-$(date --iso-8601=seconds).tgz --storage-class=ONEZONE_IA
