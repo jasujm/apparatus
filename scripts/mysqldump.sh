e2cd3826f2aeebd3bf7fcd29e0949ccf61fbcef5
Jaakko Moisio <jaakko@moisio.fi>
2021-07-10 11:11:15 +0300

    Update scripts/mysqldump.sh

--- scripts/mysqldump.sh
+++ scripts/mysqldump.sh
@@ -2,4 +2,4 @@
 
 source $(dirname $0)/include/functions.sh
 
-docker-compose exec -T mysql mysqldump -u$(< $APPARATUS_SECRETS_DIR/mysql/mysql-apparatus-user) -p$(< $APPARATUS_SECRETS_DIR/mysql/mysql-apparatus-password) --all-databases
+docker-compose exec -T mysql mysqldump --no-tablespaces -u$(< $APPARATUS_SECRETS_DIR/mysql/mysql-apparatus-user) -p$(< $APPARATUS_SECRETS_DIR/mysql/mysql-apparatus-password) apparatus
