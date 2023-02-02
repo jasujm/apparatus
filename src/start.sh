f393d7b473c114c37cb92982c599302cd8d5f791
Jaakko Moisio <jaakko@moisio.fi>
2023-02-02 21:50:47 +0200

    Remove mysql from production build

--- src/start.sh
+++ src/start.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Wait for mysql before attempting to migrate and un the app
-waittime=1
-until nc -z -w30 mysql 3306; do
-    echo 'Waiting for mysql...'
-    sleep $waittime;
-    waittime=$((waittime*2))
-    waittime=$((waittime<32 ? waittime : 32))
-done
-
 apparatus_media_dir=/var/www/apparatus/media
 if [ ! -d $apparatus_media_dir ]; then
     mkdir -p $apparatus_media_dir
